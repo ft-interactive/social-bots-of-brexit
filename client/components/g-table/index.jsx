9de82c1e4b28e6bbcf56b05db48aa61001466088
David Blood <davidbjourno@users.noreply.github.com>
2017-06-07 22:36:38 +0100

    Make table behave responsively

--- client/components/g-table/index.jsx
+++ client/components/g-table/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Table, Column, Cell } from 'fixed-data-table-2';
+import throttle from 'lodash/throttle';
 import ChartCell from './chart-cell/index.jsx';
 
 class GTable extends Component {
@@ -11,10 +12,25 @@ class GTable extends Component {
       data: props.data,
       rowHeight: 68,
       headerHeight: 42,
+      pageWidth: 0,
     };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleResize();
+
+    window.addEventListener('resize', throttle(this.handleResize, 250));
+  }
+
+  handleResize() {
+    this.setState({
+      pageWidth: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+    });
   }
 
   render() {
+    const tableWidth = this.state.pageWidth >= 568 ? 433 : 290;
     const accountNameColumn = (
       <Column
         header={
@@ -32,7 +48,7 @@ class GTable extends Component {
     const sparklineColumn = (
       <Column
         header={
-          <Cell className="header-cell">Tweet volume Jan 2016-May 2017</Cell>
+          <Cell className="header-cell">Tweets per day Jan 2016-May 2017</Cell>
         }
         cell={props => (
           <ChartCell
@@ -40,33 +56,37 @@ class GTable extends Component {
             data={this.state.data[props.rowIndex].timeseries}
           />
         )}
-        width={131}
+        width={136}
       />
     );
 
-    const tweetsPerDayColumn = (
-      <Column
-        header={
-          <Cell className="header-cell">Avg. tweets per day Jun 17-23, 2016</Cell>
-        }
-        cell={props => (
-          <Cell
-            {...props}
-            style={{ textAlign: 'right' }}
-          >
-            {this.state.data[props.rowIndex].meanperdayeuref}
-          </Cell>
-        )}
-        width={143}
-      />
-    );
+    let tweetsPerDayColumn;
+
+    if (this.state.pageWidth >= 568) {
+      tweetsPerDayColumn = (
+        <Column
+          header={
+            <Cell className="header-cell">Avg. tweets per day Jun 17-23, 2016</Cell>
+          }
+          cell={props => (
+            <Cell
+              {...props}
+              style={{ textAlign: 'right' }}
+            >
+              {this.state.data[props.rowIndex].meanperdayeuref}
+            </Cell>
+          )}
+          width={143}
+        />
+      );
+    }
 
     return (
       <Table
         rowsCount={this.state.data.length}
         rowHeight={this.state.rowHeight}
         headerHeight={this.state.headerHeight}
-        width={428}
+        width={tableWidth}
         height={(this.state.data.length * this.state.rowHeight) + this.state.headerHeight + 2}
       >
         {accountNameColumn}
